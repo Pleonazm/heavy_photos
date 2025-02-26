ecd74b8fd31a763fc7674a47cf45f730e404ff67
Pleonazm <pleonazm@gmail.com>
2025-02-26 23:17:39 +0100

    Storage, added computing Sha256 Hash for loaded data

--- hp_storage.py
+++ hp_storage.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass, field
 from io import BytesIO
 from pathlib import Path
+from functools import wraps
+
 import requests
+import hashlib
 
 class StorageError(Exception):
     """Exception for Storage problems
@@ -21,11 +24,28 @@ class Storage():
     save_method:str = ''
     load_method:str = ''
     configs:dict[dict] = field(default_factory=dict)
+    hash:str = None
 
     def configure_s3(self, aws_access_key_id, aws_secret_access_key, aws_endpoint_url_s3):
         self.configs['aws_s3']['aws_access_key_id'] = aws_access_key_id
         self.configs['aws_s3']['aws_secret_access_key'] = aws_secret_access_key
         self.configs['aws_s3']['aws_endpoint_url_s3'] = aws_endpoint_url_s3
+
+    def compute_hash(method):
+        """Decorator that computes SHA-256 hash after executing the decorated method."""
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            # Execute the original method
+            result = method(self, *args, **kwargs)
+
+            # Compute hash only if self.raw_data is set
+            if hasattr(self, 'raw_data') and isinstance(self.raw_data, bytes):
+                self.hash = hashlib.sha256(self.raw_data).hexdigest()
+            else:
+                raise AttributeError("self.raw_data is not set or is not of type bytes.")
+            
+            return result  # Return the original method's result
+        return wrapper
 
     def save_file(self, save_path):
         save_path = Path(save_path)
@@ -50,7 +70,7 @@ class Storage():
             save_functions[self.save_method](*args, **kwargs)
         else:
             raise StorageError('Wrong Save Function')
-
+    @compute_hash
     def load_http_get(self,url):
         resp = requests.get(url=url)
         resp.raise_for_status()
@@ -60,7 +80,7 @@ class Storage():
         # else:
         #     self.raw_data = resp.content
 
-
+    @compute_hash
     def load_file(self, load_path):
         load_path = Path(load_path)
         with open(load_path, mode='rb') as f:
@@ -78,4 +98,4 @@ class Storage():
         if self.load_method in load_functions:
             load_functions[self.load_method](*args, **kwargs)
         else:
-            raise StorageError('Wrong Load Function')  
+            raise StorageError('Wrong Load Function')
