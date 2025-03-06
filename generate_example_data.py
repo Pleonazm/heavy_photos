0f34bc1d534299a1017a5eed0b4604e91bab51f7
Pleonazm <pleonazm@gmail.com>
2025-03-06 22:13:34 +0100

    Example Data generation updated to include HP Controller dumps

--- generate_example_data.py
+++ generate_example_data.py
@@ -11,6 +11,7 @@ from hp_storage import DataStoragePhysical
 from ai_client_wrapper import AI_Client_Call_Wrapper
 from img_descriptor import ImgDescriptor
 from hp_data_link import JSONDataLink
+from hp_controller import HPController
 
 
 # import requests
@@ -90,13 +91,13 @@ if __name__ == '__main__':
     print('START')
 
     #local
-    # test_img_desc(i_desc_local, "example_data/storage_index_local.json")
+    test_img_desc(i_desc_local, "example_data/storage_index_local_embed.json")
     #remote, needs http server to work
-    test_img_desc(i_desc_remote, "example_data/storage_index_remote.json")
+    # test_img_desc(i_desc_remote, "example_data/storage_index_remote.json")
 
 
 
-    #Low Level Print
+    #Low Level Print No need?
 
     # # Remove Raw Data for printing
     # for s in s_obs:
@@ -106,3 +107,39 @@ if __name__ == '__main__':
     # pprint(ready_urls)
     # pprint(s_obs)
     # pprint(i_desc)
+
+    # b = benedict(Path('example_data/3b42e886e2651bd70047b2131762ed646052fa33efdea12579c70011e76026c4.png'))
+    # b['embeddings'] = None
+    # pprint(b)
+
+    # Controller Full Dump test
+
+    c = HPController()
+    test_data = [{"id": "3b42e886e2651bd70047b2131762ed646052fa33efdea12579c70011e76026c4", "uri": "example_data/dino.png", "load_method": "file"},
+                
+                {"id": "c3545bf0088c0e993f74e4fbe3000dc68660819006ab91187f2a36c1caa81211", "uri": "example_data/monk.jpg", "load_method": "file"}]
+
+    dino_det = benedict("example_data/3b42e886e2651bd70047b2131762ed646052fa33efdea12579c70011e76026c4.json", format='json')
+    monk_det = benedict('example_data/c3545bf0088c0e993f74e4fbe3000dc68660819006ab91187f2a36c1caa81211.json', format='json')
+
+    ds = DataStoragePhysical(load_method='file', uri='example_data/dino.png')
+    ms = DataStoragePhysical(load_method='file', uri='example_data/monk.jpg')
+
+    # idd = ImgDescriptor()
+
+    c.objects_list = [
+        ImgDescriptor(embeddings=dino_det['embeddings'],id=dino_det['id'], mime=dino_det['mime'], description=dino_det['description'], tags=dino_det['tags'],img_storage=ds),
+        ImgDescriptor(embeddings=monk_det['embeddings'],id=monk_det['id'], mime=monk_det['mime'], description=monk_det['description'], tags=monk_det['tags'],img_storage=ms)
+                   
+        ]
+
+    for o in c.objects_list:
+        o.img_storage.load()
+
+    fd = c.create_full_dump()
+
+    # with open("example_data/full_dump.json", "w") as f:
+    #     json.dump(fd, f, indent=4)
+
+    c2 = HPController()
+    c2.load_full_dump(fd)
